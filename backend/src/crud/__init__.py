c341980f09be7e74df06da1cd0d17e9ba31316f5
Ivan Mihun <vsk76090@gmail.com>
2025-03-06 11:32:07 +0100

    feat(backend): flatten crud imports

---
+++ backend/src/crud/__init__.py
@@ -0,0 +1,9 @@
+from .categories import CategoryCRUD
+from .products import ProductCRUD
+from .reviews import ReviewsCRUD
+from .users import UserCRUD
+from .carts import CartCRUD
+from .addresses import AddressCRUD
+from .orders import OrderCRUD
+from .tokens import RecoveryTokenCRUD, RefreshTokenCRUD, ConfirmationTokenCRUD
+from .payments import PaymentCRUD
