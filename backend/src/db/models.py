69502997df74a79777527e6b68f133770f7b2d7f
Ivan Mihun <vsk76090@gmail.com>
2025-03-08 12:21:03 +0100

    refactor(backend): remove redundancy in model definitions

--- backend/src/db/models.py
+++ backend/src/db/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime, UTC
 
-from sqlalchemy import Integer, String, TIMESTAMP, ForeignKey, Float, Boolean, Enum, Table, Column
+from sqlalchemy import Integer, String, TIMESTAMP, ForeignKey, Boolean, Table, Column
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, declared_attr
 from sqlalchemy.orm import mapped_column
@@ -12,39 +12,39 @@ from src.db.db import Base
 product_category_association = Table(
     'product_category_association', Base.metadata,
     Column('product_id', Integer, ForeignKey('products.id'), primary_key=True),
-    Column('category_name', String, ForeignKey('categories.name'), primary_key=True)
+    Column('category_name', String(30), ForeignKey('categories.name'), primary_key=True)
 )
 
 
 class User(Base):
     __tablename__ = 'users'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    password: Mapped[str] = mapped_column(String, nullable=False)
-    username: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=lambda: datetime.now(UTC),
-                                                 nullable=False)
-    is_admin: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
-    is_confirmed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column(String(50))
+    username: Mapped[str] = mapped_column(String(30))
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=False),
+                                                 default=lambda: datetime.now(UTC).replace(tzinfo=None))
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_confirmed: Mapped[bool] = mapped_column(Boolean, default=False)
 
     reviews: Mapped[list["Review"]] = relationship('Review')
 
 
 class Address(Base):
     __tablename__ = 'addresses'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
-    fullname: Mapped[str] = mapped_column(String, nullable=False)
-    country: Mapped[str] = mapped_column(String, nullable=False)
-    city: Mapped[str] = mapped_column(String, nullable=False)
-    street: Mapped[str] = mapped_column(String, nullable=False)
-    zipcode: Mapped[str] = mapped_column(String, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    fullname: Mapped[str]
+    country: Mapped[str]
+    city: Mapped[str]
+    street: Mapped[str]
+    zipcode: Mapped[str]
 
 
 class TokenBase(Base):
     __abstract__ = True
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), primary_key=True)
-    token: Mapped[str] = mapped_column(String, nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), primary_key=True)
+    token: Mapped[str] = mapped_column(String)
 
 
 class RefreshToken(TokenBase):
@@ -61,8 +61,8 @@ class ConfirmationToken(TokenBase):
 
 class ItemBase(Base):
     __abstract__ = True
-    product_id: Mapped[int] = mapped_column(Integer, ForeignKey('products.id'), primary_key=True)
-    quantity: Mapped[int] = mapped_column(Integer, default=1, nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'), primary_key=True)
+    quantity: Mapped[int] = mapped_column(default=1)
 
     @declared_attr
     def product(cls) -> Mapped["Product"]:
@@ -75,12 +75,12 @@ class ItemBase(Base):
 
 class CartItem(ItemBase):
     __tablename__ = 'cart_items'
-    cart_id: Mapped[int] = mapped_column(Integer, ForeignKey('carts.user_id'), primary_key=True)
+    cart_id: Mapped[int] = mapped_column(ForeignKey('carts.user_id'), primary_key=True)
 
 
 class Cart(Base):
     __tablename__ = 'carts'
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), primary_key=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), primary_key=True)
 
     items: Mapped[list["CartItem"]] = relationship('CartItem', lazy="selectin", cascade="all, delete-orphan")
 
@@ -109,18 +109,18 @@ class Cart(Base):
 
 class OrderItem(ItemBase):
     __tablename__ = 'order_items'
-    order_id: Mapped[int] = mapped_column(Integer, ForeignKey('orders.id'), primary_key=True)
+    order_id: Mapped[int] = mapped_column(ForeignKey('orders.id'), primary_key=True)
 
 
 class Order(Base):
     __tablename__ = 'orders'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=lambda: datetime.now(UTC),
-                                                 nullable=False)
-    status: Mapped[OrderStatus] = mapped_column(Enum(OrderStatus), default=OrderStatus.PENDING, nullable=False)
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
-    address_id: Mapped[int] = mapped_column(Integer, ForeignKey('addresses.id'))
-    is_paid: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    status: Mapped[OrderStatus] = mapped_column(default=OrderStatus.PENDING)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    address_id: Mapped[int] = mapped_column(ForeignKey('addresses.id'))
+    is_paid: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=False),
+                                                 default=lambda: datetime.now(UTC).replace(tzinfo=None))
 
     address: Mapped["Address"] = relationship('Address', uselist=False)
     items: Mapped[list["OrderItem"]] = relationship('OrderItem', lazy='selectin')
@@ -139,13 +139,13 @@ class Order(Base):
 class Product(Base):
     __tablename__ = 'products'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String, nullable=False)
-    description: Mapped[str] = mapped_column(String, nullable=False)
-    full_price: Mapped[float] = mapped_column(Float, nullable=False)
-    discount: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=lambda: datetime.now(UTC),
-                                                 nullable=False)
-    enabled: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(50))
+    description: Mapped[str] = mapped_column(String(1000))
+    full_price: Mapped[float]
+    discount: Mapped[int] = mapped_column(default=0)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=False),
+                                                 default=lambda: datetime.now(UTC).replace(tzinfo=None))
+    enabled: Mapped[bool] = mapped_column(Boolean, default=True)
 
     categories: Mapped[list["Category"]] = relationship('Category',
                                                         back_populates='products',
@@ -164,33 +164,36 @@ class Product(Base):
 
 class Category(Base):
     __tablename__ = 'categories'
-    name: Mapped[str] = mapped_column(String, primary_key=True)
-    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=lambda: datetime.now(UTC),
-                                                 nullable=False)
+    name: Mapped[str] = mapped_column(String(30), primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=False),
+                                                 default=lambda: datetime.now(UTC).replace(tzinfo=None))
 
-    products: Mapped[list["Product"]] = relationship('Product', back_populates='categories', lazy="selectin",
+    products: Mapped[list["Product"]] = relationship('Product',
+                                                     back_populates='categories',
+                                                     lazy="selectin",
                                                      secondary=product_category_association)
 
 
 class Review(Base):
     __tablename__ = 'reviews'
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'))
-    product_id: Mapped[int] = mapped_column(Integer, ForeignKey('products.id'))
-    rating: Mapped[int] = mapped_column(Integer, nullable=False)
-    content: Mapped[str] = mapped_column(String, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=lambda: datetime.now(UTC),
-                                                 nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'))
+    rating: Mapped[int]
+    content: Mapped[str] = mapped_column(String(1000))
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=False),
+                                                 default=lambda: datetime.now(UTC).replace(tzinfo=None))
 
 
 class Payment(Base):
     __tablename__ = 'payments'
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    order_id = Column(Integer, ForeignKey('orders.id'), nullable=False)
-    amount = Column(Float, nullable=False)
-    currency = Column(String(3), nullable=False)
-    payment_method = Column(String, nullable=False)
-    intent_id = Column(String, unique=True, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), default=lambda: datetime.now(UTC))
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    order_id: Mapped[int] = mapped_column(ForeignKey('orders.id'))
+    amount: Mapped[float]
+    currency: Mapped[str] = mapped_column(String(3))
+    payment_method: Mapped[str] = mapped_column(String, nullable=False)
+    intent_id: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=False),
+                                                 default=lambda: datetime.now(UTC).replace(tzinfo=None))
