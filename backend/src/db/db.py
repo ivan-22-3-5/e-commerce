ba973e79b61d2a6201d998e832c74bd5469838fd
Ivan Mihun <vsk76090@gmail.com>
2025-03-20 21:02:25 +0100

    feat(backend): add AsyncAttrs mixin to the models Base class

--- backend/src/db/db.py
+++ backend/src/db/db.py
@@ -1,12 +1,15 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
 engine = create_async_engine(settings.POSTGRESQL_DB_URL)
 
 SessionLocal = async_sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
-Base = declarative_base()
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
 
 
 async def get_db():
