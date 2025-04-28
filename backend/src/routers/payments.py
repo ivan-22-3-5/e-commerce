280735904bd23c61198122a9c899ddd080390220
Ivan Mihun <vsk76090@gmail.com>
2025-04-28 18:00:39 +0200

    feat(backend): mark order as paid in case of checkout success

--- backend/src/routers/payments.py
+++ backend/src/routers/payments.py
@@ -3,8 +3,7 @@ from fastapi import APIRouter, status, Request
 from fastapi.responses import RedirectResponse
 
 from src.config import settings
-from src.crud import OrderCRUD, PaymentCRUD
-from src.db.models import Payment
+from src.crud import OrderCRUD
 from src.deps import SessionDep
 from src.custom_exceptions import PaymentGatewayError
 
@@ -49,29 +48,22 @@ async def webhook(req: Request, db: SessionDep):
 
 async def handle_event(event, db):
     match event.type:
-        case "payment_intent.succeeded":
+        case "checkout.session.completed":
             await handle_payment_succeeded(event, db)
         case "payment_intent.payment_failed":
             await handle_payment_failed(event)
 
 
 async def handle_payment_succeeded(event, db):
-    intent = event.data.object
-    metadata = intent['metadata']
+    session = event.data.object
+    metadata = session['metadata']
+
     if order_id := metadata.get('order_id'):
         order = await OrderCRUD.get(int(order_id), db)
         order.is_paid = True
-        await PaymentCRUD.create(Payment(
-            user_id=int(metadata.get('user_id')),
-            order_id=int(order_id),
-            amount=float(intent['amount']) / 100,
-            currency=intent['currency'],
-            payment_method=intent['payment_method'],
-            intent_id=intent['id']
-        ), db)
 
 
 async def handle_payment_failed(event):
     intent = event.data.object
     error_message = intent['last_payment_error']['message'] if intent.get('last_payment_error') else None
-    print("Failed: ", intent['id'], error_message)
+    logger.info("Failed: ", intent['id'], error_message)
