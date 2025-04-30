ef53971a03684e66101e10ebb5e1688e75ada8dc
Ivan Mihun <vsk76090@gmail.com>
2025-04-30 19:14:14 +0200

    fix(backend): return float total price instead of integer in CartOut schema

--- backend/src/schemas/cart.py
+++ backend/src/schemas/cart.py
@@ -1,13 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_serializer
 
 from src.schemas.item import ItemOut, Item
 
 
 class Cart(BaseModel):
     items: list[Item]
-    total_price: float
+    total_price: int
 
 
 class CartOut(BaseModel):
     items: list[ItemOut]
-    total_price: float
+    total_price: int
+
+    @field_serializer('total_price')
+    def serialize_total_price(self, value) -> float:
+        return round(value / 100, 2)
