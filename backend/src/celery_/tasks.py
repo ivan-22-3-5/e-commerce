2a5c00d74622b04284fbcb5f8d7868bc09ccef4f
Ivan Mihun <vsk76090@gmail.com>
2025-05-04 18:51:30 +0200

    feat(backend): give a unique name to celery tasks

--- backend/src/celery_/tasks.py
+++ backend/src/celery_/tasks.py
@@ -6,13 +6,13 @@ from .utils import send_email
 celery = Celery("celery", broker=celery_config.CELERY_BROKER_URL, backend=celery_config.CELERY_BACKEND_URL)
 
 
-@celery.task
+@celery.task(name='send_password_recovery_email')
 def send_password_recovery_email(username, link, email_address):
     send_email(email_address, subject="Password recovery", template_name="password_recovery.html",
                username=username, link=link)
 
 
-@celery.task
+@celery.task(name='send_confirmation_code_email')
 def send_confirmation_code_email(code, email_address):
     send_email(email_address, subject="Confirmation code", template_name="confirmation_code.html",
                code=code)
