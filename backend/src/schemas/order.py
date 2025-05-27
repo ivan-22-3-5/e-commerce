76d10a6450ad35d05a1fc01f409d3c97185d205f
Ivan Mihun <vsk76090@gmail.com>
2025-05-27 14:01:32 +0200

    fix(order): add field serializer to convert total_price to float

--- backend/src/schemas/order.py
+++ backend/src/schemas/order.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_serializer
 
 from src.custom_types import OrderStatus
 from src.schemas.item import ItemOut, ItemIn
@@ -14,5 +14,9 @@ class OrderOut(BaseModel):
     id: int
     status: OrderStatus
     created_at: datetime
-    total_price: float
+    total_price: int
     items: list[ItemOut]
+
+    @field_serializer('total_price')
+    def convert_price_to_float(self, v: int) -> float:
+        return round(v / 100, 2)
